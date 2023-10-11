2fc999b063b97f3f67b3e244fb53cf52e06eab49
veggie-garden <devcseo@gmail.com>
2023-10-11 21:30:23 +0900

    feat: :sparkles: update main

--- main.cpp
+++ main.cpp
@@ -1,62 +1,48 @@
+#include "ConfigChecker.hpp"
+#include "ConfigMaker.hpp"
 #include "Directive.hpp"
 
-RootConfig RealConfigParser::somefunction() {
-  std::string configStr = FileReader::read(filepath);
-  ConfigLexer lexer(configStr);
-  if (lexer.fail())
-    return NULL;
-
-  BlockDirective rootDirective = lexer.getRootRedirective();
-
-  ConfigParser parser(rootDirective);
-  if (parser.fail())
-    return NULL;
-
-  RootConfig config = parser.getRootConfig();
-
-  return config;
-}
-
 int main() {
-  RealConfigParser parser("path");
 
-  if (parser.fail())
-    return 1;
+  Directive http("http");
+  // Directive root("root");
+  // root.values.push_back("/www");
+  // http.children.push_back(root);
+  Directive server("server");
+  // Directive server_root("root");
+  // server_root.values.push_back("/www");
+  // server.children.push_back(server_root);
+  // Directive server_name("server_name");
+  // server_name.values.push_back("test");
+  // server.children.push_back(server_name);
+  // Directive client_max_content_size("client_max_content_size");
+  // client_max_content_size.values.push_back("50m");
+  // server.children.push_back(client_max_content_size);
+  Directive listen("listen");
+  listen.values.push_back("8080");
+  server.children.push_back(listen);
+  Directive location("location");
+  location.values.push_back("/");
+  Directive location_return("return");
+  location_return.values.push_back("301");
+  location_return.values.push_back("/www/error.html");
+  location.children.push_back(location_return);
+  Directive accept("accept_methods");
+  accept.values.push_back("POST");
+  accept.values.push_back("GET");
+  accept.values.push_back("DELETE");
+  location.children.push_back(accept);
+  Directive cgi1("cgi_extension");
+  cgi1.values.push_back(".php");
+  cgi1.values.push_back("/bin/php");
+  location.children.push_back(cgi1);
+  Directive cgi2("cgi_extension");
+  cgi2.values.push_back(".py");
+  cgi2.values.push_back("/bin/python3");
+  location.children.push_back(cgi2);
+  server.children.push_back(location);
+  http.children.push_back(server);
 
-  RootConfig config = parser.somefunction();
-
-  -- -- -- --
+  Checker::checkDirective(http);
+  ConfigMaker::makeConfig(http);
 }
-
-// Directive, Config, RealConfigParser -> junmkang
-// ConfigLexer -> yonshin
-// ConfigParse -> cheseo
-
-// int main()
-// {
-// 	BlockDirective server1;
-// 	server1.key = "server";
-// 	server1.children.push_back(SimpleDirective("port", "8080"));
-// 	server1.children.push_back(SimpleDirective("server_name", "localhost"));
-
-// 	BlockDirective server2;
-// 	server2.key = "server";
-// 	server2.children.push_back(SimpleDirective("port", "8080"));
-// 	server2.children.push_back(SimpleDirective("server_name", "localhost"));
-
-// 	BlockDirective server3;
-// 	server3.key = "server";
-// 	server3.children.push_back(SimpleDirective("port", "8080"));
-// 	server3.children.push_back(SimpleDirective("server_name", "localhost"));
-
-// 	BlockDirective http;
-// 	http.key = "http";
-// 	http.children.push_back(BlockDirective(server1));
-// 	http.children.push_back(BlockDirective(server2));
-// 	http.children.push_back(BlockDirective(server3));
-
-// 	BlockDirective directive;
-// 	directive.key = "root";
-// 	directive.children.push_back(SimpleDirective("client_max_content_size",
-// "50m")); 	directive.children.push_back(BlockDirective(http));
-// }
