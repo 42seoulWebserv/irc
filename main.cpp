3bf091289f5af084a0f73dc37e844187179d0990
veggie-garden <devcseo@gmail.com>
2023-10-13 14:42:33 +0900

    feat: :sparkles: add more input

--- main.cpp
+++ main.cpp
@@ -8,39 +8,55 @@ int main() {
   // Directive root("root");
   // root.values.push_back("/www");
   // http.children.push_back(root);
+
   Directive server("server");
-  // Directive server_root("root");
-  // server_root.values.push_back("/www");
-  // server.children.push_back(server_root);
-  // Directive server_name("server_name");
-  // server_name.values.push_back("test");
-  // server.children.push_back(server_name);
-  // Directive client_max_content_size("client_max_content_size");
-  // client_max_content_size.values.push_back("50m");
-  // server.children.push_back(client_max_content_size);
+
+  // server_name
+  Directive server_name("server_name");
+  server_name.values.push_back("test");
+  server.children.push_back(server_name);
+
+  // client_max_content_size
+  Directive client_max_content_size("client_max_content_size");
+  client_max_content_size.values.push_back("50m");
+  server.children.push_back(client_max_content_size);
+
+  // listen
   Directive listen("listen");
   listen.values.push_back("8080");
   server.children.push_back(listen);
+
+  // location
   Directive location("location");
   location.values.push_back("/");
+
   Directive location_return("return");
   location_return.values.push_back("301");
   location_return.values.push_back("/www/error.html");
   location.children.push_back(location_return);
+
+  Directive location_index("index");
+  location_index.values.push_back("index.html");
+  location.children.push_back(location_index);
+
   Directive accept("accept_methods");
   accept.values.push_back("POST");
   accept.values.push_back("GET");
   accept.values.push_back("DELETE");
   location.children.push_back(accept);
+
   Directive cgi1("cgi_extension");
   cgi1.values.push_back(".php");
   cgi1.values.push_back("/bin/php");
   location.children.push_back(cgi1);
+
   Directive cgi2("cgi_extension");
   cgi2.values.push_back(".py");
   cgi2.values.push_back("/bin/python3");
   location.children.push_back(cgi2);
+
   server.children.push_back(location);
+
   http.children.push_back(server);
 
   Checker::checkDirective(http);
