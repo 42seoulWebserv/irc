425111195a94cc17d32b73988d4d57b97c92f339
moaoh <k010103@naver.com>
2023-11-10 22:22:16 +0900

    feat: :tada: init put method

--- src/processor/MethodPutProcessor.cpp
+++ src/processor/MethodPutProcessor.cpp
@@ -1,11 +1,32 @@
 #include "MethodPutProcessor.hpp"
 
-MethodPutProcessor::MethodPutProcessor(const RequestVO& request,
-                                       const LocationConfig* config, int kq,
-                                       IObserver<ResponseVO>* ob)
+MethodPutProcessor::MethodPutProcessor(const RequestVO &request,
+                                       const LocationConfig *config, int kq,
+                                       IObserver<ResponseVO> *ob)
     : ob_(ob) {
   (void)request;
   (void)config;
   (void)kq;
   (void)ob;
+  // conf 읽는 함수
+  FileReadEventController::addEventController(kq, "default.conf", this);
+}
+
+// conf 쓰는 함수 (내용을 수정할 파트)
+void MethodPutProcessor::onEvent(const FileReadEventController::Event &event,
+                                 int kq) {
+  // if (event.type_ == FileReadEventController::???) {
+  //   ob_->onEvent(ResonseVO(404));
+  //   return;
+  // }
+  // std::string content = request.getBody();
+  std::string content;
+  FileWriteEventController::addEventController(kq, "default.conf", content,
+                                               this);
+}
+
+// 읽은 결과를 ResponseVO로 만드는 함수 (성공 실패 등등)
+void MethodPutProcessor::onEvent(const FileWriteEventController::Event &event) {
+  ResponseVO response;
+  ob_->onEvent(response);
 }
--- src/processor/MethodPutProcessor.hpp
+++ src/processor/MethodPutProcessor.hpp
@@ -1,18 +1,25 @@
 #ifndef MethodPutProcessor_HPP_
 #define MethodPutProcessor_HPP_
 
+#include "FileReadEventController.hpp"
+#include "FileWriteEventController.hpp"
 #include "IObserver.hpp"
 #include "IRequestProcessor.hpp"
 #include "LocationConfig.hpp"
 #include "RequestVO.hpp"
 #include "ResponseVO.hpp"
 
-class MethodPutProcessor : public IRequestProcessor {
- public:
+class MethodPutProcessor : public IRequestProcessor,
+                           IObserver<FileReadEventController::Event>,
+                           IObserver<FileWriteEventController::Event> {
+public:
   MethodPutProcessor(const RequestVO &request, const LocationConfig *config,
                      int kq, IObserver<ResponseVO> *ob);
 
- private:
+  void onEvent(const FileReadEventController::Event &event, int kq);
+  void onEvent(const FileWriteEventController::Event &event);
+
+private:
   IObserver<ResponseVO> *ob_;
 };
 
