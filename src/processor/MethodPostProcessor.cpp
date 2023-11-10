7d0fa2e7e8b58be643dbc3b6e88d8e620698292a
moaoh <k010103@naver.com>
2023-11-10 22:20:12 +0900

    feat: :tada: init post method

--- src/processor/MethodPostProcessor.cpp
+++ src/processor/MethodPostProcessor.cpp
@@ -1,11 +1,27 @@
 #include "MethodPostProcessor.hpp"
 
-MethodPostProcessor::MethodPostProcessor(const RequestVO& request,
-                                         const LocationConfig* config, int kq,
-                                         IObserver<ResponseVO>* ob)
+#include "FileWriteEventController.hpp"
+#include "ResponseVO.hpp"
+
+MethodPostProcessor::MethodPostProcessor(const RequestVO &request,
+                                         const LocationConfig *config, int kq,
+                                         IObserver<ResponseVO> *ob)
     : ob_(ob) {
+
+  std::string filename = request.getUri();
+  std::string content = request.getBody();
+
+  std::cout << "in MethodPostProcessor" << std::endl;
+  FileWriteEventController::addEventController(kq, filename, content, this);
   (void)request;
   (void)config;
-  (void)kq;
   (void)ob;
+}
+
+// 받은 내용들을 바탕으로 작성할 파트
+void MethodPostProcessor::onEvent(
+    const FileWriteEventController::Event &event) {
+  ResponseVO response;
+
+  this->ob_->onEvent(response);
 }
--- src/processor/MethodPostProcessor.hpp
+++ src/processor/MethodPostProcessor.hpp
@@ -1,18 +1,21 @@
 #ifndef MethodPostProcessor_HPP_
 #define MethodPostProcessor_HPP_
 
+#include "FileWriteEventController.hpp"
 #include "IObserver.hpp"
 #include "IRequestProcessor.hpp"
 #include "LocationConfig.hpp"
 #include "RequestVO.hpp"
 #include "ResponseVO.hpp"
 
-class MethodPostProcessor : public IRequestProcessor {
- public:
+class MethodPostProcessor : public IRequestProcessor,
+                            IObserver<FileWriteEventController::Event> {
+public:
   MethodPostProcessor(const RequestVO &request, const LocationConfig *config,
                       int kq, IObserver<ResponseVO> *ob);
+  void onEvent(const FileWriteEventController::Event &event);
 
- private:
+private:
   IObserver<ResponseVO> *ob_;
 };
 
