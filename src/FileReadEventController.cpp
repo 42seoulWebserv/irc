97e7ed1ec67b8f4ffe3ef1e8c751a9ea8f54562e
yonshin <yonshin@student.42seoul.kr>
2023-11-11 02:49:32 +0900

    feat: refine FileReadEventController error code

--- src/FileReadEventController.cpp
+++ src/FileReadEventController.cpp
@@ -2,17 +2,31 @@
 
 #include <fcntl.h>
 #include <sys/event.h>  // kevent
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 #include <exception>
+#include <fstream>
 
 FileReadEventController::FileReadEventController(int kq,
                                                  const std::string &filepath,
                                                  IObserver<Event> *observer)
     : kq_(kq), filepath_(filepath), observer_(observer) {
-  fd_ = open(filepath_.c_str(), O_RDONLY);
+  const char *file = filepath.c_str();
+  struct stat statbuf;
+  if (stat(file, &statbuf)) {
+    throw FileReadException(FileReadEventController::NOT_FOUND);
+  }
+  if (access(file, R_OK)) {
+    throw FileReadException(FileReadEventController::NOT_ACCESS);
+  }
+  if (statbuf.st_size == 0) {
+    throw FileReadException(FileReadEventController::SUCCESS);
+  }
+  fd_ = open(file, O_RDONLY);
   if (fd_ == -1) {
-    throw std::invalid_argument("file open error");
+    throw FileReadException(FileReadEventController::FAIL);
   }
   fcntl(fd_, F_SETFL, O_NONBLOCK);
   fcntl(fd_, F_SETFD, FD_CLOEXEC);
@@ -26,10 +40,13 @@ void FileReadEventController::addEventController(int kq_,
                                                  IObserver<Event> *observer) {
   try {
     new FileReadEventController(kq_, filepath, observer);
-  } catch (...) {
-    if (observer) {
-      observer->onEvent(Event(FileReadEventController::FAIL, ""));
+  } catch (const FileReadException &e) {
+    if (observer == NULL) {
+      return;
     }
+    observer->onEvent(Event(e.type_, ""));
+  } catch (...) {
+    observer->onEvent(Event(FileReadEventController::FAIL, ""));
   }
 }
 
@@ -60,3 +77,6 @@ EventController::returnType FileReadEventController::handleEvent(
 FileReadEventController::Event::Event(EventType type,
                                       const std::string &content)
     : type_(type), content_(content) {}
+
+FileReadEventController::FileReadException::FileReadException(EventType type)
+    : type_(type) {}
--- src/FileReadEventController.hpp
+++ src/FileReadEventController.hpp
@@ -3,17 +3,24 @@
 
 #define FILE_READ_BUFF_SIZE 16384
 
+#include <exception>
+
 #include "EventController.hpp"
 #include "IObserver.hpp"
 
 class FileReadEventController : public EventController {
  public:
-  enum EventType { SUCCESS, FAIL };
+  enum EventType { SUCCESS, FAIL, NOT_FOUND, NOT_ACCESS };
   class Event {
    public:
     Event(enum EventType type, const std::string &content);
     enum EventType type_;
     const std::string &content_;
+  };
+  class FileReadException : public std::exception {
+   public:
+    FileReadException(EventType type);
+    enum EventType type_;
   };
   static void addEventController(int kq_, const std::string &filepath,
                                  IObserver<Event> *observer);
