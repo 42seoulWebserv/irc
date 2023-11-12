b39dd81bb1db9ece367ef5ba03efb0d2dd9323c0
sindydwns <sindydwns@naver.com>
2023-11-13 00:25:43 +0900

    Revert "test: FilePath.cpp"
    This reverts commit 656ed4a0edcd4b0b1bd6312e5598d8fc19e929da.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <assert.h>
 #include <sys/event.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -11,93 +10,9 @@
 
 #include "ConfigLexer.hpp"
 #include "ConfigMaker.hpp"
-#include "FilePath.hpp"
 #include "RootConfig.hpp"
 #include "ServerConfig.hpp"
 #include "ServerEventController.hpp"
-
-void testFilePath() {
-  {  // constructor test
-    FilePath root("/");
-    assert(root.getPath() == "/");
-    root.appendPath("test");
-    assert(root.getPath() == "/test");
-
-    FilePath current2("");
-    assert(current2.getPath() == "");
-    current2.appendPath("test");
-    assert(current2.getPath() == "test");
-  }
-  {  // appendPath test
-    FilePath path("/test1");
-    assert(path.getPath() == "/test1");
-    path.appendPath("test2");
-    assert(path.getPath() == "/test1/test2");
-    path.appendPath("test3");
-    assert(path.getPath() == "/test1/test2/test3");
-    path.appendPath("/test4");
-    assert(path.getPath() == "/test1/test2/test3/test4");
-    path.appendPath("/test5/");
-    assert(path.getPath() == "/test1/test2/test3/test4/test5/");
-  }
-  {  // isExist test
-    FilePath makefile("Makefile");
-    assert(makefile.isExist());
-
-    FilePath longFilePath("we-are-project-name-is-irc-but-it-is-not-irc");
-    assert(!longFilePath.isExist());
-  }
-  {  // isDirectory, isFile test
-    FilePath srcDir("src");
-    assert(srcDir.isDirectory());
-    assert(!srcDir.isFile());
-
-    FilePath srcDirWithSlash("src/");
-    assert(srcDirWithSlash.isDirectory());
-    assert(!srcDir.isFile());
-
-    FilePath makefile("Makefile");
-    assert(!makefile.isDirectory());
-    assert(makefile.isFile());
-
-    FilePath makefileWithSlash("Makefile/");
-    assert(!makefileWithSlash.isDirectory());
-    assert(!makefileWithSlash.isFile());
-  }
-  {  // toDirectoryPath, toFilePath test
-    FilePath path2("src");
-    assert(path2.toFilePath() == "src");
-    assert(path2.toDirectoryPath() == "src/");
-
-    FilePath path1("src/");
-    assert(path1.toFilePath() == "src");
-    assert(path1.toDirectoryPath() == "src/");
-
-    FilePath longPath1("is/not/irc/");
-    assert(longPath1.toFilePath() == "is/not/irc");
-    assert(longPath1.toDirectoryPath() == "is/not/irc/");
-
-    FilePath longPath2("is/not/irc");
-    assert(longPath2.toFilePath() == "is/not/irc");
-    assert(longPath2.toDirectoryPath() == "is/not/irc/");
-  }
-  {  // getExtension, getFileName, getDirectory test
-    FilePath path1("src/main.cpp");
-    assert(FilePath::getExtension(path1) == "cpp");
-    assert(FilePath::getFileName(path1) == "main.cpp");
-    assert(FilePath::getDirectory(path1) == "src");
-
-    FilePath path2("src/");
-    assert(FilePath::getExtension(path2) == "");
-    assert(FilePath::getFileName(path2) == "");
-    assert(FilePath::getDirectory(path2) == "src");
-
-    FilePath path3("src");
-    assert(FilePath::getExtension(path3) == "");
-    assert(FilePath::getFileName(path3) == "src");
-    assert(FilePath::getDirectory(path3) == "");
-  }
-}
 
 int run(RootConfig &config) {
   int kq = kqueue();
@@ -170,10 +85,8 @@ int main(int argc, char **argv) {
 
   Directive directive = ConfigLexer::run(configStr);
   RootConfig config = ConfigMaker::makeConfig(directive);
-  // config.printRootConfig();
+  config.printRootConfig();
 
-  // run(config);
-  testFilePath();
-
+  run(config);
   return 0;
 }
