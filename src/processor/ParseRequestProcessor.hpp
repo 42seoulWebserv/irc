cf2b799e2f6b543e5bc6c4a47aa31c2eebbf48e4
yonshin <yonshin@student.42seoul.kr>
2023-11-18 13:25:10 +0900

    doing~

---
+++ src/processor/ParseRequestProcessor.hpp
@@ -0,0 +1,10 @@
+#ifndef ParseRequestProcessor_HPP_
+#define ParseRequestProcessor_HPP_
+
+#include "IRequestProcessor.hpp"
+
+class ParseRequestProcessor : public IRequestProcessor {
+ public:
+};
+
+#endif
