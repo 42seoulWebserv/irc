24bb65f74710f7b24171081bf3752c59d641bbe1
moaoh <k010103@naver.com>
2023-11-20 21:16:02 +0900

    feat: :sparkles: implementing POST method

--- src/processor/MethodPostProcessor.cpp
+++ src/processor/MethodPostProcessor.cpp
@@ -1,5 +1,39 @@
 #include "MethodPostProcessor.hpp"
 
-MethodPostProcessor::MethodPostProcessor(IClient &client) : client_(client) {}
+#include <fstream>
+#include <iostream>
 
-ProcessResult MethodPostProcessor::process() { return ProcessResult(); }
+#include "FilePath.hpp"
+
+MethodPostProcessor::MethodPostProcessor(IClient &client) : client_(client) {
+  std::cout << "in POST Method" << std::endl;
+}
+
+static void writePostFile(FilePath &filepath, std::string &content) {
+  std::ofstream outputFile(filepath + "post.txt");
+  if (!outputFile.is_open()) {
+    std::cerr << "Error: POST: std::ofstream" << std::endl;
+  }
+  outputFile << content;
+  outputFile.close();
+}
+
+ProcessResult MethodPostProcessor::process() {
+  std::cout << "in POST process" << std::endl;
+  FilePath filepath = "." + client_.getLocationConfig()->getRootPath();
+  filepath.append(client_.getRequest().getUri());
+  filepath = filepath.toDirectoryPath();
+  std::cout << "filepath: " << filepath << std::endl;
+  std::string content = client_.getRequest().getBody();
+  if (!filepath.isExist()) {  // error code
+    std::cout << "directory exists: " << filepath << std::endl;
+  } else {
+    writePostFile(filepath, content);
+  }
+  FileWriteEventController::addEventController(filepath, content, this);
+  return ProcessResult();
+}
+
+void MethodPostProcessor::onEvent(const FileWriteEventController::Event &p) {
+  this->onEvent(p);
+}
--- src/processor/MethodPostProcessor.hpp
+++ src/processor/MethodPostProcessor.hpp
@@ -1,19 +1,23 @@
 #ifndef MethodPostProcessor_HPP_
 #define MethodPostProcessor_HPP_
 
+#include "FileWriteEventController.hpp"
 #include "IObserver.hpp"
 #include "IRequestProcessor.hpp"
 #include "LocationConfig.hpp"
 #include "Request.hpp"
 #include "Response.hpp"
 
-class MethodPostProcessor : public IRequestProcessor {
+class MethodPostProcessor : public IRequestProcessor,
+                            IObserver<FileWriteEventController::Event> {
  public:
   MethodPostProcessor(IClient &client);
   ProcessResult process();
+  void onEvent(const FileWriteEventController::Event &p);
 
  private:
   IClient &client_;
+  FileWriteEventController *writer;
 };
 
 #endif
