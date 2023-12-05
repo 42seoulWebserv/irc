cad21930bad81c16b8154e68f383388c288afa3c
sindydwns <sindydwns@naver.com>
2023-12-05 16:39:50 +0900

    fix: redirect response error

--- src/processor/RedirectionProcessor.cpp
+++ src/processor/RedirectionProcessor.cpp
@@ -9,11 +9,10 @@ RedirectionProcessor::RedirectionProcessor(IClient& client) : client_(client) {
 ProcessResult RedirectionProcessor::process() {
   int status = client_.getLocationConfig()->getRedirectionStatusCode();
   std::string path = client_.getLocationConfig()->getRedirectionPath();
-  Response response = client_.getResponse();
   client_.setResponseStatusCode(status);
   client_.setResponseHeader("Location", path);
   client_.setResponseHeader("Content-Length", "0");
-  client_.getDataStream().readStr(response.toString());
+  client_.getDataStream().readStr(client_.getResponse().toString());
   client_.getDataStream().setEof(true);
   return ProcessResult().setWriteOn(true).setNextProcessor(new WaitProcessor());
 }
